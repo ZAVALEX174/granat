d90eca18dce582ffadc1352f777532d2958f8662
Alexandr Zuev <zuevalex@yandex.ru>
2024-06-25 22:14:54 +0500

    на работе

--- script/choose-swiper.js
+++ script/choose-swiper.js
@@ -1,16 +1,14 @@
 const chooseSlider = new Swiper('.choose__swiper', {
-    loop: true,
+        loop: true,
         speed: 1500,
-        spaceBetween: 10,
-    // effect: "fade", //вряд ли, тк не будет видно второго слайда
         slidesPerView: 1.25,
-        autoHeight: true,
+        // autoHeight: true,
         centeredSlides: true,
         centeredSlidesBounds: true,
 
+
     pagination: {
         el: '.choose__swiper-pagination',
-        // dynamicBullets: true,
         clickable: true,
         type: 'progressbar',
     },
@@ -18,21 +16,35 @@ const chooseSlider = new Swiper('.choose__swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-        effect: "creative",
-        loopedSlides:2,
-        creativeEffect: {
-            limitProgress: 2,
-            prev: {
-                translate: ["-100vw", 0, 0],
-                opacity: 0,
-                scale: 0
-            },
-            next: {
-                translate: ["65%", 0, 0],
-                opacity: 1, //0.4
-                scale: .75
-            }
+
+
+    effect: "creative",
+    loopedSlides:2,
+    creativeEffect: {
+        limitProgress: 2,
+        prev: {
+            translate: ["-100vw", 0, 0],
+            opacity: 0,
+            scale: 0
+        },
+        next: {
+            translate: ["65%", 0, 0],
+            opacity: 1, //0.4
+            scale: 1,
         }
-}
-);
+    },
+
+    // breakpoint: {
+    //         320: {
+    //             slidesPerView: 1,
+    //             spaceBetween: 100,
+    //         },
+    //         1000: {
+    //
+    //         },
+    //
+    // },
+
+
+});
 
