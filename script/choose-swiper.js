f06646b05f162b5ef39cd4e06ed6802e6f725e20
Alexander <zuevalex14021983@gmail.com>
2024-06-26 08:10:54 +0500

    на работе(choose-swiper переход с deck на mob)

--- script/choose-swiper.js
+++ script/choose-swiper.js
@@ -6,44 +6,56 @@ const chooseSlider = new Swiper('.choose__swiper', {
         centeredSlides: true,
         centeredSlidesBounds: true,
 
-
-    pagination: {
-        el: '.choose__swiper-pagination',
-        clickable: true,
-        type: 'progressbar',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-
-    effect: "creative",
-    loopedSlides:2,
-    creativeEffect: {
-        limitProgress: 2,
-        prev: {
-            translate: ["-100vw", 0, 0],
-            opacity: 0,
-            scale: 0
+        pagination: {
+            el: '.choose__swiper-pagination',
+            clickable: true,
+            type: 'progressbar',
         },
-        next: {
-            translate: ["65%", 0, 0],
-            opacity: 1, //0.4
-            scale: 1,
-        }
-    },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
 
-    // breakpoint: {
-    //         320: {
-    //             slidesPerView: 1,
-    //             spaceBetween: 100,
-    //         },
-    //         1000: {
-    //
-    //         },
-    //
-    // },
+        effect: "creative",
+        loopedSlides:2,
+        creativeEffect: {
+            limitProgress: 2,
+            prev: {
+                translate: ["-100vw", 0, 0],
+                opacity: 0,
+                scale: 0
+            },
+            next: {
+                translate: ["65%", 0, 0],
+                opacity: 1, //0.4
+                scale: 1,
+            }
+        },
+
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 100,
+
+                creativeEffect: {
+                    limitProgress: 2,
+                    prev: {
+                        translate: ["-100vw", 0, 0],
+                        opacity: 0,
+                        scale: 0
+                    },
+                    next: {
+                        translate: ["100%", 0, 0],
+                        opacity: 1, //0.4
+                        scale: 1,
+                    }
+                },
+            },
+            1000: {
+
+
+            },
+        },
 
 
 });
